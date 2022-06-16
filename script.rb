f7e547e23628e38ee6f43c3d99fa577fa923bdbd
Celus8 <celus217@gmail.com>
2022-06-16 21:52:14 +0200

    Make computer able to guess randomly and human able to make code

--- script.rb
+++ script.rb
@@ -1,38 +1,61 @@
-class Game
-  attr_reader :code
-
-  def initialize
-    @code = [rand(1..9), rand(1..9), rand(1..9), rand(1..9)].join
+module Checker
+  def valid?(input)
+    input_array = input.split('')
+    input_array.all? { |value| integer?(value) } && input_array.length == 4
   end
 
-  def play
-    puts 'Welcome to Mastermind! A 4 digit code will be generated, with numbers 1-9, both included. Inputting a 0 or text will terminate the program. You have 12 tries to guess the code. Good luck!'
-    tries = 12
+  def integer?(value)
+    value.to_i.to_s == value
+  end
+end
+
+class Game
+  include Checker
+
+  attr_reader :code, :guess, :code_maker, :code_guesser
+
+  def initialize
+    puts 'Welcome to Mastermind! Enter "make" if you want to be the codemaker, or "guess" if you want to be the codeguesser. The computer will play the other role.'
     loop do
-      guess = gets.chomp
-      break if guess.to_i.zero?
-
-      result = check(guess)
-      break if result == 'Win'
-
-      unless result
-        puts 'Incorrect input!'
-        next
-      end
-      tries -= 1
-      puts "You have #{result[0]} correct digits in the correct place, and #{result[1]} correct digits in an incorrect place. You have #{tries} tries left."
-      if tries <= 0
-        puts 'You ran out of tries. You lose!'
+      case gets.chomp
+      when 'make'
+        @code_maker = Human.new
+        @code_guesser = Computer.new
+        puts 'Input the secret code you want to generate, and the computer will try to guess it. The code has to be made of 4 digits between 1 and 9, both included.'
         break
+      when 'guess'
+        @code_guesser = Human.new
+        @code_maker = Computer.new
+        puts 'A 4 digit code will be generated, with numbers 1-9, both included. Inputting a 0 will terminate the program. You have 12 tries to guess the code. Good luck!'
+        break
+      else
+        puts 'Incorrect input!'
       end
     end
   end
 
-  def check(player_guess)
-    return false unless valid?(player_guess)
+  def play
+    @code = code_maker.generate_code
+    tries = 12
+    loop do
+      @guess = code_guesser.make_guess
+      break unless @guess
 
+      result = check(guess)
+      break if result == 'Win'
+
+      tries -= 1
+      puts "#{code_guesser} #{result[0]} correct digits in the correct place, and #{result[1]} correct digits in an incorrect place. #{code_guesser} #{tries} tries left."
+      if tries <= 0
+        puts "#{code_guesser} ran out of tries. #{code_guesser} lost!"
+        break
+      end
+      code_guesser.pause
+    end
+  end
+  def check(player_guess)
     if player_guess == @code
-      puts 'You win!'
+      puts "#{code_guesser} won!"
       return 'Win'
     end
     guess_array = player_guess.split('')
@@ -44,15 +67,55 @@ class Game
     white = (guess_array & code_array).length - red
     [red, white]
   end
+end
 
-  def valid?(input)
-    input_array = input.split('')
-    input_array.all? { |value| integer?(value) } && input_array.length == 4
+class Human
+  include Checker
+
+  def generate_code
+    loop do
+      provisional_code = gets.chomp
+      if valid?(provisional_code)
+        return provisional_code
+      else
+        puts 'Incorrect code format! Try again.'
+      end
+    end
   end
-
-  def integer?(value)
-    value.to_i.to_s == value
+  def make_guess
+    loop do
+      provisional_guess = gets.chomp
+      if valid?(provisional_guess)
+        return provisional_guess
+      elsif provisional_guess == '0'
+        puts 'Game over'
+        return false
+      else
+        puts 'Incorrect guess format! Try again.'
+      end
+    end
+  end
+  def pause
+  end
+  def to_s
+    'You have'
   end
 end
 
-Game.new.play
+class Computer
+  def generate_code
+    [rand(1..9), rand(1..9), rand(1..9), rand(1..9)].join
+  end
+  def make_guess
+    [rand(1..9), rand(1..9), rand(1..9), rand(1..9)].join
+  end
+  def to_s
+    'The computer has'
+  end
+  def pause
+    sleep 2
+  end
+end
+
+game = Game.new
+game.play
